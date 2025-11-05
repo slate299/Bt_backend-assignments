56c771d0580081672a939b477eb0386424ca17d9
Natasha <natashahinga58@gmail.com>
2025-11-05 22:47:11 +0300

    add assignment1_intro

---
+++ assignment1_intro/intro.js
@@ -0,0 +1,11 @@
+// intro.js
+
+// Function to display user introduction details
+function introduce(name, age, language) {
+  console.log(`Name: ${name}`);
+  console.log(`Age: ${age}`);
+  console.log(`Favourite Programming Language: ${language}`);
+}
+
+// Calling the function
+introduce("Natasha Hinga", 19, "JavaScript");
